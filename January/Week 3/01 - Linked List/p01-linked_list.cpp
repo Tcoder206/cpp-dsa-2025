00434e532d4168214588f7db63c1ee6b567e3a1a
Tcoder206 <proglang704206@gmail.com>
2025-01-20 23:20:39 +0700

    Linked List: Remove Element

--- January/Week 3/01 - Linked List/p01-linked_list.cpp
+++ January/Week 3/01 - Linked List/p01-linked_list.cpp
@@ -46,7 +46,7 @@ void insert(node **head, int x, int k) {
     node *newNode = makeNode(x);
     node *temp = *head;
     int n = countNode(*head);
-    if(k < 1 || k > n) return;
+    if(k < 1 || k > n + 1) return;
     if(k == 1) {
         pushFront(head, x);
         return;
@@ -57,19 +57,55 @@ void insert(node **head, int x, int k) {
     newNode->next = temp->next;
     temp->next = newNode;
 }
+void popFront(node **head) {
+    node *temp = *head;
+    if(*head == nullptr) return;
+    *head = (*head)->next;
+    delete temp;
+}
+void popBack(node **head) {
+    node *temp = *head;
+    if(*head == nullptr) return;
+    if(temp->next == nullptr) {
+        *head = nullptr;
+        delete temp;
+        return;
+    }
+    while(temp->next->next != nullptr) {
+        temp = temp->next;
+    }
+    node *last = temp->next;
+    temp->next = nullptr;
+    delete last;
+}
+void popNode(node **head, int k) {
+    node *temp = *head;
+    int size = countNode(*head);
+    if(k < size || k > size + 1) return;
+    if(*head == nullptr) return;
+    for(int i = 1; i <= k - 2; i++) {
+        temp = temp->next;
+    }
+    node *other = temp->next;
+    temp->next = other->next;
+    delete other;
+}
 void menu() {
     cout << endl << "------------------------------" << endl;
-    cout << "1. Them o dau tien" << endl;
-    cout << "2. Them o giua" << endl;
-    cout << "3. Them o cuoi cung" << endl;
-    cout << "4. Duyet phan tu" << endl;
+    cout << "1. Them phan tu o dau tien" << endl;
+    cout << "2. Them phan tu o giua" << endl;
+    cout << "3. Them phan tu o cuoi cung" << endl;
+    cout << "4. Xoa phan tu dau tien" << endl;
+    cout << "5. Xoa phan tu o giua" << endl;
+    cout << "6. Xoa phan tu cuoi cung" << endl;
+    cout << "7. Duyet phan tu" << endl;
     cout << "0. Thoat" << endl;
     cout << "------------------------------" << endl;
     cout << "Nhap lua chon: ";
 }
 int main() {
     node *head = nullptr;
-    bool run = true;//
+    bool run = true;
     while(run) {
         menu();
         int n; cin >> n;
@@ -90,6 +126,16 @@ int main() {
                 insert(&head, x2, k);
                 break;
             case 4:
+                popFront(&head);
+                break;
+            case 5:
+                int l; cout << "Nhap doi so: "; cin >> l;
+                popNode(&head, l);
+                break;
+            case 6:
+                popBack(&head);
+                break;
+            case 7:
                 cout << "Linked list: ";
                 duyetNode(head);
                 break;
