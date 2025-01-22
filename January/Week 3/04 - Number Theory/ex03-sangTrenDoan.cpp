dfbbb7cce38b692dde0c678effe2172d527ae4d6
Tcoder206 <proglang704206@gmail.com>
2025-01-22 23:23:13 +0700

    Number Theory PI: ex02 - ex04

--- January/Week 3/04 - Number Theory/ex03-sangTrenDoan.cpp
+++ January/Week 3/04 - Number Theory/ex03-sangTrenDoan.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+void sangPrime(int l, int r) {
+    int prime[r - l + 1];
+    memset(prime, 1, sizeof(prime));
+    for(int i = 2; i <= sqrt(r); i++) {
+        for(int j = max(i * i, (l + i - 1)/i * i); j <= r; j += i) {
+            prime[j - l] = 0;
+        }
+    }
+    for(int i = max(l, 2); i <= r; i++) {
+        if(prime[i - l]) cout << i << " ";
+    }
+}
 int main() {
-    int n; cin >> n;
+    int l, r; cin >> l >> r;
+    sangPrime(l, r);
     return 0;
 }
