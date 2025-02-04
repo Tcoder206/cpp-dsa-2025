aa1aa5a790df23e1640b6f936c8eef52d71491fd
Tcoder206 <proglang704206@gmail.com>
2025-02-04 19:49:48 +0700

    Number Theory Pt I

---
+++ Topic 1 - Basic Number Theory/Phan03 - Tong Hop/Bai06-DemUocSoDuong.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("TEST.INP", "r", stdin);
+    freopen("TEST.OUT", "w", stdout);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        int cnt = 0;
+        for(int i = 1; i <= sqrt(n); i++) {
+            if(n % i == 0) {
+                if(i == sqrt(n)) cnt++;
+                else cnt += 2;
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
