d6e16c38d187115e245209d60dabbe568088771f
Tcoder206 <proglang704206@gmail.com>
2025-02-05 22:42:49 +0700

    Graph And DSU

---
+++ Topic 04 - Graph/BFS/f-cycle.cpp
@@ -0,0 +1,48 @@
+/*
+V E
+9 9
+1 2
+1 3
+1 5
+2 4
+3 6
+3 7
+3 9
+5 8
+8 9
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool visited[100];
+int parent[100];
+vector<vector<int>> arr;
+bool bfs(int k) {
+    memset(visited, false, sizeof(visited));
+    queue<int> temp;
+    temp.push(k);
+    visited[k] = true;
+    while(!temp.empty()) {
+        int top = temp.front();
+        temp.pop();
+        for(int u : arr[top]) {
+            if(!visited[u]) {
+                temp.push(u);
+                parent[u] = top;
+                visited[u] = true;
+            } else if(u != parent[top]) return true;
+        }
+    }
+    return false;
+}
+int main() {
+    int n, m; cin >> n >> m;
+    arr.resize(n + 1);
+    for(int i = 0; i < m; i++) {
+        int x, y; cin >> x >> y;
+        arr[x].push_back(y);
+        arr[y].push_back(x);
+    }
+    bool cycle = bfs(1);
+    cout << boolalpha << cycle << endl;
+    return 0;
+}
