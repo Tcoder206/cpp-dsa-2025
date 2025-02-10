d0126d5e0b09ef55ec7873530e1b2204448ca9b2
Tcoder206 <proglang704206@gmail.com>
2025-02-10 23:57:56 +0700

    Practise basic DP

---
+++ Topic 07 - Dynamic Programming/knapsack.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("TEST.INP", "r", stdin);
+    freopen("TEST.OUT", "w", stdout);
+    int n, m; cin >> n >> m;
+    int dp[n + 1][m + 1];
+    int w[n], v[n];
+    for(int &i : w) cin >> i;
+    for(int &i : v) cin >> i;
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= m; j++) {
+            if(i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i - 1][j];
+                if(j > w[i - 1]) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+                }
+            }
+        }
+    }
+    return 0;
+}
